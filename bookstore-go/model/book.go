65e9891b21c18d9ee6ad5b8a16b477a6c9614141
Miot <imuy@foxmail.com>
2025-08-26 20:16:14 +0800

    Initial commit

---
+++ bookstore-go/model/book.go
@@ -0,0 +1,24 @@
+package model
+
+import "time"
+
+type Book struct {
+	ID          int       `json:"id"`
+	Title       string    `json:"title"`
+	Author      string    `json:"author"`
+	Price       int       `json:"price"`
+	Discount    int       `json:"discount"`
+	Type        string    `json:"type"`
+	Stock       int       `json:"stock"`
+	Status      int       `json:"status"` // 上架1，下架0
+	Description string    `json:"description"`
+	CoverUrl    string    `json:"cover_url"`
+	ISBN        string    `json:"isbn"`
+	Publisher   string    `json:"publisher"`
+	Pages       int       `json:"pages"`
+	Language    string    `json:"language"`
+	Format      string    `json:"format"` // 装帧
+	CategoryID  int       `json:"category_id"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}
